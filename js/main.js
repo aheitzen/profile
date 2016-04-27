ae545238baaf5a020247c2f55cc9c4a57c0d9deb
aheitzen <alexaheitzenrader@yahoo.com>
2016-04-26 19:58:29 -0700

    added jQuery

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("I work");
+$(document).ready(function (){
+            $("#projectbutton").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#projects").offset().top
+                }, 2000);
+            });
+        });
+
+$(document).ready(function (){
+            $("#contactbutton").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#footer").offset().top
+                }, 2000);
+            });
+        });
